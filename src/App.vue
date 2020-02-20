8dec893b61443d54fd30b8cd4361688821379e0e
coolinovo <3104272@qq.com>
2020-02-20 22:33:44 +0800

    todos

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,93 @@
 <template>
   <div id="app">
-    <h1>Hello</h1>
-    <count-sub></count-sub>
-    <hr style="margin: 20px 0">
-    <count-add></count-add>
-    <hr>
-    <p>{{$store.getters.showNum}}</p>
-    <p>{{showNum}}</p>
+    <a-input
+        placeholder="请输入任务"
+        class="my-ipt"
+        :value="inputVal"
+        @change="iptChange"
+    ></a-input>
+    <a-button type="primary" @click="addItem">添加事项</a-button>
+
+    <a-list bordered :dataSource="list" class="dt-list">
+            <a-list-item slot="renderItem" slot-scope="item">
+              <!-- 复选框 -->
+              <a-checkbox :checked="item.done" @change="changeCkb($event, item.id)">{{item.info}}</a-checkbox>
+              <!-- 删除链接 -->
+              <a slot="actions" @click="del(item.id)">删除</a>
+            </a-list-item>
+
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务 -->
+        <span>0条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button type="primary">全部</a-button>
+          <a-button type="warning">未完成</a-button>
+          <a-button>已完成</a-button>
+        </a-button-group>
+        <!-- 清空已完成 -->
+        <a>清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-  import Subtraction from "./components/Subtraction";
-  import Addition from "./components/Addition";
-  import { mapGetters } from 'vuex'
+  import { mapState } from 'vuex'
 
   export default {
     name: 'app',
-    components: {
-      'count-sub': Subtraction,
-      'count-add': Addition
+    data() {
+      return {}
     },
     computed: {
-      ...mapGetters([ 'showNum' ])
+      ...mapState(['list', 'inputVal'])
+    },
+    created() {
+      this.$store.dispatch("getList")
+    },
+    methods: {
+      iptChange(e) {
+        console.log(e)
+        this.$store.commit('changeIpt', e.target.value)
+      },
+      // 添加
+      addItem() {
+        if (this.inputVal.trim().length <= 0) return this.$message.warning('不能为空')
+        this.$store.commit('addItem')
+      },
+      // 删除一条
+      del(id) {
+        this.$store.commit('delItem', id)
+      },
+      // 复选框
+      changeCkb(e, id) {
+        const checked = e.target.checked
+        this.$store.commit('changeCkb', {checked, id})
+      }
     }
   }
 </script>
 
 <style scoped>
+  #app {
+    user-select: none;
+    padding: 10px;
+  }
 
+  .my-ipt {
+    width: 500px;
+    margin-right: 10px;
+  }
+
+  .dt-list {
+    width: 500px;
+    margin-top: 10px;
+  }
+
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,53 @@
-import Vue from 'vue'
+import Vue from  'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  // 需要全局共享的数据
   state: {
-    count: 0
+    list: [],
+    // 文本框内容
+    inputVal: '',
+    nextId: 5
   },
   mutations: {
-    add(state) {
-      state.count++
+    setList(state, data) {
+      state.list = data
     },
-    addN(state, step) {
-      state.count += step
+    // 输入框变化
+    changeIpt(state, val) {
+      state.inputVal = val
+    },
+    // 添加
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputVal,
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputVal = ''
+    },
+    // 删除
+    delItem(state, id) {
+      // 根据 id 查找索引
+      const i = state.list.findIndex(x => x.id === id)
+      // 删除
+      state.list.splice(i, 1)
+    },
+    changeCkb(state, info) {
+      console.log(info)
+      const i = state.list.findIndex(x => x.id === info.id)
+      console.log(i)
+      state.list[i].done = info.checked
     }
   },
   actions: {
-    addAsync(context, step) {
-      setTimeout(() => {
-        context.commit('addN', step)
-      }, 1000)
+    async getList(context) {
+      const {data} = await axios.get('/list.json')
+      context.commit('setList', data)
     }
-  },
-  getters: {
-    showNum(state) {
-      return `当前最新的数据是[${state.count}]`
-    }
-  },
-  modules: {
   }
 })
